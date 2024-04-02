1a3ccddea39fea6294596a19bd54810a85545a0a
carol <carolina_brandes_ext@carrefour.com>
2024-04-02 19:53:40 -0300

    feat/install and config react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { Button } from "./components/ui/button"
+import { RouterProvider } from "react-router-dom"
 import "./global.css"
+import { router } from "./routes"
 
 export function App() {
 
   return (
-    <div className="flex justify-center items-center h-screen">
-     <Button>enviar</Button>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,8 @@
+
+export  function Dashboard() {
+  return (
+    <div>
+      Dashboard
+    </div>
+  )
+}
