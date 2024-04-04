f55ede7e61b5f419f173aac53bb2dfce5193857d
carol <carolina_brandes_ext@carrefour.com>
2024-04-04 14:50:01 -0300

    feat/sign-in/App: add toast from sonner lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { Helmet, HelmetProvider } from "react-helmet-async"
-import { RouterProvider } from "react-router-dom"
-import "./global.css"
-import { router } from "./routes"
+import './global.css'
+
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
+
+import { router } from './routes'
 
 export function App() {
-
   return (
-   <HelmetProvider>
-    <Helmet titleTemplate="%s | pizza.shop" />
-     <RouterProvider router={router} />
-   </HelmetProvider>
+    <HelmetProvider>
+      <Toaster richColors />
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
-
-
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -20,8 +21,19 @@ export function SignIn() {
   } = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
-    console.log('🚀 ~ handleSignIn ~ data:', data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      console.log('🚀 ~ handleSignIn ~ data:', data)
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch (error) {
+      toast.error('Credenciais inválidas')
+    }
   }
 
   return (
