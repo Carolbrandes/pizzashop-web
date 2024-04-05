34b99dd8d3437a3384dc9c498490eb98c28a88ab
carol <carolina_brandes_ext@carrefour.com>
2024-04-05 11:21:25 -0300

    feat/Add theme toggle, and account menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
+import { AccountMenu } from './account-menu'
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
 import { Separator } from './ui/separator'
 
 export function Header() {
@@ -18,6 +20,11 @@ export function Header() {
           <UtensilsCrossed className="h-4 w-4" /> Pedidos{' '}
         </NavLink>
       </nav>
+
+      <div className="ml-auto flex items-center gap-2">
+        <ThemeToggle />
+        <AccountMenu />
+      </div>
     </div>
   )
 }
