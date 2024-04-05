b39a0b339f45f88b0ee893335a29932fd4ac63bb
carol <carolina_brandes_ext@carrefour.com>
2024-04-05 10:55:16 -0300

    feat/add component header

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 
-export  function AppLayout() {
-  return (
-    <div>
-      <h1>cabeçalho</h1>
+import { Header } from '@/components/header'
 
-      <div>
+export function AppLayout() {
+  return (
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
+
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
     </div>
