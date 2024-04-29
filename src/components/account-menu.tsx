850c441079fbd1cbb20358ed15115b59d58e93cc
carol <carolina.brandes@corebiz.ag>
2024-04-29 10:00:11 -0300

    feat/query update-profile and apply staleTime: Infinity in queries profile and managedRestaurant

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,12 +1,12 @@
-import { useQuery } from '@tanstack/react-query'
-import { Building, ChevronDown, LogOut } from 'lucide-react'
+import { useQuery } from "@tanstack/react-query";
+import { Building, ChevronDown, LogOut } from "lucide-react";
 
-import { getManagedRestaurant } from '@/api/get-managed-restaurants'
-import { getProfile } from '@/api/get-profile'
+import { getManagedRestaurant } from "@/api/get-managed-restaurants";
+import { getProfile } from "@/api/get-profile";
 
-import { StoreProfileDialog } from './store-profile-dialog'
-import { Button } from './ui/button'
-import { Dialog, DialogTrigger } from './ui/dialog'
+import { StoreProfileDialog } from "./store-profile-dialog";
+import { Button } from "./ui/button";
+import { Dialog, DialogTrigger } from "./ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -14,63 +14,72 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from './ui/dropdown-menu'
-import { Skeleton } from './ui/skeleton'
+} from "./ui/dropdown-menu";
+import { Skeleton } from "./ui/skeleton";
 
 export function AccountMenu() {
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
-    queryKey: ['profile'], //* essa key identifica a requisicao, entao se em outro componente eu fizer essa mesma query, ele nao vai fazer a chamada de novo.
+    queryKey: ["profile"], //* essa key identifica a requisicao, entao se em outro componente eu fizer essa mesma query, ele nao vai fazer a chamada de novo.
     queryFn: getProfile,
-  })
+    staleTime: Infinity, //* nao vai recarregar os dados qd usuario sai e da foco na pagina
+  });
 
-  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant  } = useQuery({
-    queryKey: ['managed-restaurant'],
-    queryFn: getManagedRestaurant,
-  })
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryKey: ["managed-restaurant"],
+      queryFn: getManagedRestaurant,
+      staleTime: Infinity,
+    });
 
   return (
-   <Dialog>
-     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          className="flex select-none items-center gap-2"
-        >
-          {isLoadingManagedRestaurant ? <Skeleton className='h-4 w-40' /> : managedRestaurant?.name}
-          <ChevronDown className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            className="flex select-none items-center gap-2"
+          >
+            {isLoadingManagedRestaurant ? (
+              <Skeleton className="h-4 w-40" />
+            ) : (
+              managedRestaurant?.name
+            )}
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
 
-      <DropdownMenuContent align="end" className="w-56">
-        <DropdownMenuLabel className="flex flex-col">
-        {isLoadingProfile ? (
-          <div className='space-y-1.5'>
-            <Skeleton className='h-4 w-32' />
-            <Skeleton className='h-3 w-24' />
-          </div>
-         ) : (<>
-           <span>{profile?.name}</span>
-           <span className="text-xs font-normal text-muted-foreground">
-             {profile?.email}
-           </span>
-         </>)}
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-       <DialogTrigger asChild>
-       <DropdownMenuItem>
-          <Building className="mr-2 h-4 w-4" />
-          <span>Perfil da loja</span>
-        </DropdownMenuItem>
-       </DialogTrigger>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            {isLoadingProfile ? (
+              <div className="space-y-1.5">
+                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-3 w-24" />
+              </div>
+            ) : (
+              <>
+                <span>{profile?.name}</span>
+                <span className="text-xs font-normal text-muted-foreground">
+                  {profile?.email}
+                </span>
+              </>
+            )}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <Building className="mr-2 h-4 w-4" />
+              <span>Perfil da loja</span>
+            </DropdownMenuItem>
+          </DialogTrigger>
 
-        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-          <LogOut className="mr-2 h-4 w-4" />
-          <span>Sair</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Sair</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
       <StoreProfileDialog />
-   </Dialog>
-  )
+    </Dialog>
+  );
 }
--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,40 +1,66 @@
 import { getManagedRestaurant } from "@/api/get-managed-restaurants";
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useQuery } from "@tanstack/react-query";
+import { updateProfile } from "@/api/update-profile";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 import { Button } from "./ui/button";
 import {
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
 } from "./ui/dialog";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 
 const storeProfileSchema = z.object({
-    name: z.string().min(1),
-    description: z.string()
-})
+  name: z.string().min(1),
+  description: z.string(),
+});
 
-type StoreProfileSchema = z.infer<typeof storeProfileSchema>
+type StoreProfileSchema = z.infer<typeof storeProfileSchema>;
 
 export const StoreProfileDialog = () => {
-    const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant  } = useQuery({
-        queryKey: ['managed-restaurant'],
-        queryFn: getManagedRestaurant,
-      })
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryKey: ["managed-restaurant"],
+      queryFn: getManagedRestaurant,
+      staleTime: Infinity,
+    });
 
-      const {register, handleSubmit} = useForm<StoreProfileSchema>({
-        resolver: zodResolver(storeProfileSchema),
-        values: {
-            name: managedRestaurant?.name ?? '',
-            description: managedRestaurant?.description ?? ''
-        }
-      })
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<StoreProfileSchema>({
+    resolver: zodResolver(storeProfileSchema),
+    values: {
+      name: managedRestaurant?.name ?? "",
+      description: managedRestaurant?.description ?? "",
+    },
+  });
+
+  const { mutateAsync: updateProfilefn } = useMutation({
+    mutationFn: updateProfile,
+  });
+
+  async function handleUpdateProfile(data: StoreProfileSchema) {
+    try {
+      await updateProfilefn({
+        name: data.name,
+        description: data.description,
+      });
+
+      toast.success("Perfil atualizado com sucesso!");
+    } catch {
+      toast.error("Falha ao atualizar o perfil, tente novamente!");
+    }
+  }
 
   return (
     <DialogContent>
@@ -45,27 +71,34 @@ export const StoreProfileDialog = () => {
         </DialogDescription>
       </DialogHeader>
 
-      <form>
+      <form onSubmit={handleSubmit(handleUpdateProfile)}>
         <div className="space-y-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label className="text-right" htmlFor="name">
               Nome
             </Label>
-            <Input className="col-span-3" id="name" {...register('name')} />
+            <Input className="col-span-3" id="name" {...register("name")} />
           </div>
 
           <div className="grid grid-cols-4 items-center gap-4">
             <Label className="text-right" htmlFor="description">
               Descrição
             </Label>
-            <Textarea className="col-span-3" id="description" {...register('description')} />
+            <Textarea
+              className="col-span-3"
+              id="description"
+              {...register("description")}
+            />
           </div>
         </div>
         <DialogFooter>
-          <Button variant="ghost" type="button">
-            Cancelar
-          </Button>
-          <Button variant="sucess" type="submit">
+          <DialogClose asChild>
+            {" "}
+            <Button variant="ghost" type="button">
+              Cancelar
+            </Button>
+          </DialogClose>
+          <Button variant="sucess" type="submit" disabled={isSubmitting}>
             Salvar
           </Button>
         </DialogFooter>
