66597e7e936f980800f125e58696c8874c90afa3
carol <carolina.brandes@corebiz.ag>
2024-05-15 10:34:50 -0300

    feat/config test with vitest, testeing library and happy-dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import react from '@vitejs/plugin-react'
-import path from "node:path"
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { UserConfig, defineConfig } from "vite";
+import { InlineConfig } from "vitest";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    setupFiles: ["./test/setup.ts"],
+    environment: "happy-dom",
+  },
+} as UserConfig & {
+  test: InlineConfig;
+});
