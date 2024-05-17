99929c27a9336d0b89b2d10eb731bebd2fe3fa7c
carol <carolina.brandes@corebiz.ag>
2024-05-17 20:22:20 -0300

    feat/test order-status

--- src/components/order-status.spec.tsx
+++ src/components/order-status.spec.tsx
@@ -1,3 +1,69 @@
-test("1 + 1 equals 2", () => {
-  expect(1 + 1).toEqual(2);
+import { render } from "@testing-library/react";
+import { OrderStatus } from "./order-status";
+
+//* findAll/findBy - retorna uma promise, usando qd é o caso de um elemento que demora pra carregar e ai com esse metodo ele espera ser exibido
+
+//* getAll/getBy - procura pelo elemento e se nao existir ele vai dar erro
+
+//* queryAll/queryBy - procura pelo elemento e se nao existir ele nao vai dar erro, ele retorna null
+
+describe("Order Status", () => {
+  it("should display the right text when order status is pending", () => {
+    const wrapper = render(<OrderStatus status="pending" />);
+
+    //* debug: vai mostrar o html q é gerado
+    wrapper.debug();
+
+    const statusText = wrapper.getByText("Pendente");
+    console.log(statusText.outerHTML);
+
+    const badgeElement = wrapper.getByTestId("badge");
+
+    expect(statusText).toBeInTheDocument();
+    expect(badgeElement).toHaveClass("bg-slate-400");
+  });
+
+  it("should display the right text when order status is canceled", () => {
+    const wrapper = render(<OrderStatus status="canceled" />);
+
+    const statusText = wrapper.getByText("Cancelado");
+
+    const badgeElement = wrapper.getByTestId("badge");
+
+    expect(statusText).toBeInTheDocument();
+    expect(badgeElement).toHaveClass("bg-rose-500");
+  });
+
+  it("should display the right text when order status is delivering", () => {
+    const wrapper = render(<OrderStatus status="delivering" />);
+
+    const statusText = wrapper.getByText("Em entrega");
+
+    const badgeElement = wrapper.getByTestId("badge");
+
+    expect(statusText).toBeInTheDocument();
+    expect(badgeElement).toHaveClass("bg-amber-500");
+  });
+
+  it("should display the right text when order status is processing", () => {
+    const wrapper = render(<OrderStatus status="processing" />);
+
+    const statusText = wrapper.getByText("Em preparo");
+
+    const badgeElement = wrapper.getByTestId("badge");
+
+    expect(statusText).toBeInTheDocument();
+    expect(badgeElement).toHaveClass("bg-amber-500");
+  });
+
+  it("should display the right text when order status is delivered", () => {
+    const wrapper = render(<OrderStatus status="delivered" />);
+
+    const statusText = wrapper.getByText("Entregue");
+
+    const badgeElement = wrapper.getByTestId("badge");
+
+    expect(statusText).toBeInTheDocument();
+    expect(badgeElement).toHaveClass("bg-emerald-500");
+  });
 });
