26533203339581c2820ef2fbb78e67fa820f54cb
carol <carolbrandes23@gmail.com>
2024-05-31 16:33:43 -0300

    test(mocks): create mocks of order actions like approve, cancel and etc...

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,5 +1,9 @@
 import { env } from "@/env";
 import { setupWorker } from "msw/browser";
+import { approveOrderMock } from "./approve-order-mock";
+import { cancelOrderMock } from "./cancel-oder-mock";
+import { deliverOrderMock } from "./deliver-oder-mock";
+import { dispatchOrderMock } from "./dispatch-order-mock";
 import { getDailyRevenueInPeriodMock } from "./get-daily-revenue-in-period-mock";
 import { getDayOrdersAmountMock } from "./get-day-orders-amount";
 import { getManagedRestaurantMock } from "./get-managed-restaurant-mock";
@@ -28,6 +32,10 @@ export const worker = setupWorker(
   updateProfileMock,
   getOrdersMock,
   getOrderDetailsMock,
+  approveOrderMock,
+  cancelOrderMock,
+  deliverOrderMock,
+  dispatchOrderMock,
 );
 
 export async function enableMSW() {
