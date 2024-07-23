8efaa0136dad8969fc1a839ac145b68914e71ff7
carol <carolina.brandes@corebiz.ag>
2024-07-23 18:00:34 -0400

    test/dashboard metrics

--- src/api/mocks/update-profile-mock.ts
+++ src/api/mocks/update-profile-mock.ts
@@ -3,7 +3,7 @@ import { UpdateProfileBody } from "../update-profile";
 
 //* every time this request is called with body equal to null, it will return status 401
 //* the first parameter in http.post is the type of params (in our case we don't use so we put never), the second is type of request body and the third is the type of response
-export const updateProfileMock = http.post<never, UpdateProfileBody>(
+export const updateProfileMock = http.put<never, UpdateProfileBody>(
   "/profile",
   async ({ request }) => {
     const { name } = await request.json();
